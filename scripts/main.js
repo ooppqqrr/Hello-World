d9171a8d1ef09763380c2c785c93cc58dc81da22
ooppqqrr <ooppqqrr666@gmail.com>
2018-05-09 00:32:14 +0800

    201805091232
    201805091232

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+
+myButton.onclick = function() {
+  setUserName();
+}
+
